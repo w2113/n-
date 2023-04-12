1f4924bbc9d6a069a4fc51f0843d949a4b30b890
w2113 <756320639@qq.com>
2023-04-12 22:24:48 +0800

    .

---
+++ leetcode88合并两个有序数组/源.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+
+    int i = m - 1, j = n - 1, k = m + n - 1;
+    while (i >= 0 && j >= 0)
+    {
+        if (nums1[i] > nums2[j])
+        {
+            nums1[k--] = nums1[i--];
+
+        }
+        else
+        {
+            nums1[k--] = nums2[j--];
+        }
+    }
+    while (j >= 0)
+    {
+        nums1[k--] = nums2[j--];
+    }
+
+}
