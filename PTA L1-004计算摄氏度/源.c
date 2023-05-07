b886674162a34746273d66613b5fb1150bc64ce2
w2113 <756320639@qq.com>
2023-05-07 21:33:16 +0800

    .

---
+++ PTA L1-004计算摄氏度/源.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+#include<stdio.h>
+int main()
+{
+    int f = 0;
+    scanf("%d", &f);
+    printf("Celsius = %d", 5 * (f - 32) / 9);
+    return 0;
+}
