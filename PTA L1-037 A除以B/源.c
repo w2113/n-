8aedff7fc7d8445af83b81816c093e458cddc0a9
w2113 <756320639@qq.com>
2023-07-09 23:26:21 +0800

    .

---
+++ PTA L1-037 A除以B/源.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	int a, b;
+	float x;
+	scanf("%d%d", &a, &b);
+	x = 1.0 * a / b;
+	if (b < 0)
+		printf("%d/(%d)=%.2f", a, b, x);
+	else if (b == 0)
+		printf("%d/%d=Error", a, b);
+	else
+		printf("%d/%d=%.2f", a, b, x);
+	system("pause");
+}
