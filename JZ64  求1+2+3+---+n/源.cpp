8be1f304eec3c4ee498c6bd2bb5410d28fe6ead3
w2113 <756320639@qq.com>
2023-09-10 10:33:06 +0800

    .

---
+++ JZ64  求1+2+3+---+n/源.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+class Solution {
+    class sum
+    {
+    public:
+        sum()
+        {
+            ret += i;
+            i++;
+        }
+    };
+public:
+    int Sum_Solution(int n)
+    {
+        sum s[n];
+        return ret;
+    }
+private:
+    static int ret;
+    static int i;
+};
+int Solution::ret = 0;
+int Solution::i = 1;
